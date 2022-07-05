7c154aa197753b9efa582a7ec9d8f4b622aea3b0
Jayavarma S <85405082+Jayavarma103@users.noreply.github.com>
2022-07-05 19:17:10 +0530

    Update predict.py

--- Naive_Bayes_classifier/predict.py
+++ Naive_Bayes_classifier/predict.py
@@ -6,12 +6,6 @@ import math
 
 from validate import validate
 
-"""
-Predicts the target values for data in the file at 'test_X_file_path', using the model learned during training.
-Writes the predicted values to the file named "predicted_test_Y_nb.csv". It should be created in the same directory where this code file is present.
-This code is provided to help you get started and is NOT a complete implementation. Modify it based on the requirements of the project.
-"""
-
 def import_data_and_model(test_X_file_path, model_file_path):
     test_X = np.genfromtxt(test_X_file_path, delimiter='\n', dtype=str)
     with open(model_file_path, "r") as read_file:
@@ -73,10 +67,6 @@ def write_to_csv_file(pred_Y, predicted_Y_file_name):
 
 
 def predict(test_X_file_path):
-    # Load Model Parameters
-    """
-    Parameters of Naive Bayes include Laplace smoothing parameter, Prior probabilites of each class and values related to likelihood computation.
-    """
     test_X, model = import_data_and_model(test_X_file_path, "./TRAINED_MODEL.json")
     pred_Y = predict_target_values(test_X, model)
     write_to_csv_file(pred_Y, "predicted_test_Y_nb.csv")    
@@ -85,5 +75,4 @@ def predict(test_X_file_path):
 if __name__ == "__main__":
     test_X_file_path = sys.argv[1]
     predict(test_X_file_path)
-    # Uncomment to test on the training data
     validate(test_X_file_path, actual_test_Y_file_path="train_Y_nb.csv") 
