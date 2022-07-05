ef5790c30fb096b15833a9b1bd86a64126ebac07
Jayavarma S <85405082+Jayavarma103@users.noreply.github.com>
2022-07-05 19:20:42 +0530

    Update predict.py

--- logistic_regression/predict.py
+++ logistic_regression/predict.py
@@ -4,17 +4,10 @@ import sys
 from train import sigmoid
 from validate import validate
 
-"""
-Predicts the target values for data in the file at 'test_X_file_path', using the weights learned during training.
-Writes the predicted values to the file named "predicted_test_Y_lg.csv". It should be created in the same directory where this code file is present.
-This code is provided to help you get started and is NOT a complete implementation. Modify it based on the requirements of the project.
-"""
-
 def import_data_and_weights(test_X_file_path, weights_file_path):
     test_X = np.genfromtxt(test_X_file_path, delimiter=',', dtype=np.float64, skip_header=1)
     weights = np.genfromtxt(weights_file_path, delimiter=',', dtype=np.float64)
     return test_X, weights
-
 
 def sort_(array):
     array0, array1, array2, array3 = array[0], array[1], array[2], array[3]
@@ -42,7 +35,6 @@ def predict_target_values(test_X, weights):
     pred_Y = np.array(sort_(result))
     return pred_Y
 
-
 def write_to_csv_file(pred_Y, predicted_Y_file_name):
     pred_Y = pred_Y.reshape(len(pred_Y), 1)
     with open(predicted_Y_file_name, 'w', newline='') as csv_file:
@@ -50,15 +42,12 @@ def write_to_csv_file(pred_Y, predicted_Y_file_name):
         wr.writerows(pred_Y)
         csv_file.close()
 
-
 def predict(test_X_file_path):
     test_X, weights = import_data_and_weights(test_X_file_path, "WEIGHTS_FILE.csv")
     pred_Y = predict_target_values(test_X, weights)
     write_to_csv_file(pred_Y, "predicted_test_Y_lg.csv")
 
-
 if __name__ == "__main__":
     test_X_file_path = sys.argv[1]
     predict(test_X_file_path)
-    # Uncomment to test on the training data
     validate(test_X_file_path, actual_test_Y_file_path="train_Y_lg_v2.csv") 
